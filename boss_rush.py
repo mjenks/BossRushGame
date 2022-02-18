0197aaf4f91a794828c88330cf862895f2abb898
mjenks <rikku.hotaru@gmail.com>
2022-02-18 16:50:30 -0500

    wizard
    Added basic animation of the wizard

--- boss_rush.py
+++ boss_rush.py
@@ -25,26 +25,37 @@ pygame.display.set_caption("Boss Rush")
 FPS = 60
 PAD = 25
 BOSS_SIZE = BOSS_WIDTH, BOSS_HEIGHT = 900, 300
+CHAR_SIZE = CHAR_WIDTH, CHAR_HEIGHT = 120, 180
+CHAR_IMG_SIZE =  CHAR_WIDTH*10, CHAR_HEIGHT*10 #image has 10 down 10 across
 
-boss_count = 1
+
+frame_count = 1
 
 #Creative Commons Image obtained from opengameart.org Author: jkjkke
 BACKGROUND_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'JWDLx5AZBtI.jpg')), SIZE)
 #Creative Commons Image obtained from opengameart.org Author: inosine
 DRAGON_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'dragon.png')), BOSS_SIZE)
-
+#Creative Commons Image obtained from opengameart.org Artist: Calciumtrice
+WIZARD_IMAGES = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'wizard.png')), CHAR_IMG_SIZE)
 
 #game classes
 
 #pygame functions
 def draw_window():
-    global boss_count
+    global frame_count
     WIN.blit(BACKGROUND_IMAGE, (0,0))
     
-    boss_step = boss_count//10
+    #Animate wizard on screen
+    char_step = frame_count//10
+    char_frame = pygame.Rect(((char_step%10)*CHAR_WIDTH, 5*CHAR_HEIGHT), (CHAR_WIDTH, CHAR_HEIGHT))
+    WIN.blit(WIZARD_IMAGES, (WIDTH//4 - CHAR_WIDTH//2, HEIGHT - CHAR_HEIGHT - PAD), char_frame)
+    
+    #Animate dragon on screen
+    boss_step = frame_count//10
     boss_frame = pygame.Rect(((boss_step%3)*BOSS_WIDTH//3,0), (BOSS_WIDTH//3, BOSS_HEIGHT))
     WIN.blit(DRAGON_IMAGE, (WIDTH//2 + 3*PAD, HEIGHT - BOSS_HEIGHT - 2*PAD), boss_frame)
-    boss_count += 1
+    
+    frame_count += 1
     
     pygame.display.update()
 
