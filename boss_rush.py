9bf92e972e9c2d1b441ac4b0241c2630c16902ee
mjenks <rikku.hotaru@gmail.com>
2022-02-18 16:21:25 -0500

    added images
    Added background Image and animation for Dragon boss

--- boss_rush.py
+++ boss_rush.py
@@ -18,26 +18,44 @@ pygame.font.init()
 WHITE = (255, 255, 255)
 
 #Constants
-SIZE = WIDTH, HEIGHT = 800, 500
+SIZE = WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Boss Rush")
+
+FPS = 60
+PAD = 25
+BOSS_SIZE = BOSS_WIDTH, BOSS_HEIGHT = 900, 300
+
+boss_count = 1
+
+#Creative Commons Image obtained from opengameart.org Author: jkjkke
+BACKGROUND_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'JWDLx5AZBtI.jpg')), SIZE)
+#Creative Commons Image obtained from opengameart.org Author: inosine
+DRAGON_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'dragon.png')), BOSS_SIZE)
 
 
 #game classes
 
 #pygame functions
 def draw_window():
-    WIN.fill(WHITE)
+    global boss_count
+    WIN.blit(BACKGROUND_IMAGE, (0,0))
+    
+    boss_step = boss_count//10
+    boss_frame = pygame.Rect(((boss_step%3)*BOSS_WIDTH//3,0), (BOSS_WIDTH//3, BOSS_HEIGHT))
+    WIN.blit(DRAGON_IMAGE, (WIDTH//2 + 3*PAD, HEIGHT - BOSS_HEIGHT - 2*PAD), boss_frame)
+    boss_count += 1
     
     pygame.display.update()
 
 #pygame run loop
 def run():
     play = True
+    clock = pygame.time.Clock()
     while play:
-        
+        clock.tick(FPS)
         for event in pygame.event.get():
-            if event.type == pygame.Quit:
+            if event.type == pygame.QUIT:
                 play = False
                 pygame.quit()
         
