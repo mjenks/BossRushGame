7660f8d0e95e37ecf1afaa90de34066a109c4959
mjenks <rikku.hotaru@gmail.com>
2022-02-19 16:27:05 -0500

    end conditions
    handled boss and player death as well as player out of Mana

--- boss_rush.py
+++ boss_rush.py
@@ -18,6 +18,7 @@ pygame.font.init()
 
 #Colors
 WHITE = (255, 255, 255)
+RED = (255, 0 , 0)
 
 #Constants
 SIZE = WIDTH, HEIGHT = 900, 500
@@ -28,6 +29,7 @@ STAT_FONT = pygame.font.SysFont('comicsans', 40)
 SPELL_FONT = pygame.font.SysFont('brushscript', 25)
 SPELL_SELECTED_FONT = pygame.font.SysFont('brushscript', 25)
 SPELL_SELECTED_FONT.set_underline(True)
+MESSAGE_FONT = pygame.font.SysFont('herald', 60)
 
 FPS = 60
 PAD = 25
@@ -35,6 +37,9 @@ BOSS_SIZE = BOSS_WIDTH, BOSS_HEIGHT = 900, 300
 CHAR_SIZE = CHAR_WIDTH, CHAR_HEIGHT = 125, 150
 CHAR_IMG_SIZE =  CHAR_WIDTH*10, CHAR_HEIGHT*10 #image has 10 down 10 across
 
+BOSS_DEATH = pygame.USEREVENT + 1
+PLAYER_DEATH = pygame.USEREVENT + 2
+PLAYER_MANA_TOO_LOW = pygame.USEREVENT + 3
 
 frame_count = 1
 choice = 0
@@ -48,16 +53,34 @@ DRAGON_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('Images', '
 WIZARD_IMAGES = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'wizard.png')), CHAR_IMG_SIZE)
 
 #game functions
+def check_life(p1, dragon):
+    dead = False
+    if p1.health <= 0:
+        pygame.event.post(pygame.event.Event(PLAYER_DEATH))
+        dead = True
+    if dragon.health <= 0:
+        pygame.event.post(pygame.event.Event(BOSS_DEATH))
+        dead = True
+    return dead
+        
+        
+        
 def take_turn(p1, dragon):
     global choice
     #player casts selected spell
     p1.spells[choice](dragon)
+    if check_life(p1,dragon): return
     #Boss turn
     p1.turn_start(dragon)
+    if check_life(p1,dragon): return
     dragon.attack(p1)
+    if check_life(p1,dragon): return
     #Begining of turn effects for player
     p1.turn_start(dragon)
+    if check_life(p1,dragon): return
     p1.can_cast()
+    if len(p1.spells) == 0:
+        pygame.event.post(pygame.event.Event(PLAYER_MANA_TOO_LOW))
     choice = 0
     
     
@@ -108,6 +131,12 @@ def draw_window(p1, dragon):
     frame_count += 1
     
     pygame.display.update()
+    
+def draw_message(text):
+    draw_text = MESSAGE_FONT.render(text, 1, WHITE, RED)
+    WIN.blit(draw_text, (WIDTH//2 - draw_text.get_width()//2, HEIGHT//2 - draw_text.get_height()//2))
+    pygame.display.update()
+    pygame.time.delay(3000)
 
 #pygame run loop
 def run():
@@ -124,9 +153,12 @@ def run():
     while play:
         clock.tick(FPS)
         for event in pygame.event.get():
+            
             if event.type == pygame.QUIT:
                 play = False
+                message = "Bye"
                 pygame.quit()
+                
             if event.type == pygame.KEYDOWN:
                 if (event.key == pygame.K_UP) and choice > 0:
                     choice -= 1
@@ -134,9 +166,31 @@ def run():
                     choice += 1
                 if event.key == pygame.K_RETURN:
                     take_turn(p1, dragon)
+                    
+            if event.type == BOSS_DEATH:
+                dragons_slain += 1
+                draw_message("Boss Defeated!")
+                dragon = boss.Boss()
+                p1.turn_start(dragon)
+                
+            if event.type == PLAYER_DEATH:
+                message = "Game Over  " + str(dragons_slain) + " dragons slain")
+                dragons_slain = 0
+                play = False
+                
+            if event.type == PLAYER_MANA_TOO_LOW:
+                draw_message("Can't Cast")
+                message = "Game Over  " + str(dragons_slain) + " dragons slain"
+                dragons_slain = 0
+                play = False
         
+        if play == False:
+            draw_message(message)
+            break
         
         draw_window(p1, dragon)
+        
+    run()
 
 #run game
 if __name__ == "__main__":
